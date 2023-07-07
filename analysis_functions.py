3112e644063270caa7e146767c918db9afa3bdfe
danny <hs2722@ic.ac.uk>
2023-07-07 23:29:54 +0100

    Update analysis and app

--- analysis_functions.py
+++ analysis_functions.py
@@ -1,12 +1,17 @@
 import scanpy as sc
 import anndata
+import matplotlib.pyplot as plt
 
 def load_libraries():
     # Scanpy and other libraries are imported at the start, no need for a specific function
     pass
 
-def load_nsclc_data(filename):
-    adata = sc.read_10x_h5(filename)
+def load_nsclc_data(uploaded_file):
+    # Save uploaded file to a temporary file
+    with open("temp.h5", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    # Now you can pass the path of the temporary file to read_10x_h5()
+    adata = sc.read_10x_h5("temp.h5")
     return adata
 
 def initialize_seurat_object(adata):
@@ -14,16 +19,27 @@ def initialize_seurat_object(adata):
     # Additional preprocessing can be performed as needed
     return adata
 
-def perform_qc(adata):
+
+def perform_qc(adata, plt, sc):
+    adata.var_names_make_unique()  # add this line to make var_names unique
     adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
     sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-    return adata
+    # Calculate n_genes_by_counts
+    adata.obs['n_genes_by_counts'] = adata.X.sum(axis=1).A1    
+    # Create the violin plots
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    fig = sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], multi_panel=True)
+    
+    # plt.savefig("qc.png")
+    # plt.close()
+    return adata, fig
 
-def filter_data(adata):
-    sc.pp.filter_cells(adata, min_genes=200)
-    sc.pp.filter_genes(adata, min_cells=3)
-    adata = adata[adata.obs.n_genes_by_counts < 2500, :]
-    adata = adata[adata.obs.pct_counts_mt < 5, :]
+
+def filter_data(adata, min_genes, min_cells, max_genes, max_mt):
+    sc.pp.filter_cells(adata, min_genes=min_genes)
+    sc.pp.filter_genes(adata, min_cells=min_cells)
+    adata = adata[adata.obs.n_genes_by_counts < max_genes, :]
+    adata = adata[adata.obs.pct_counts_mt < max_mt, :]
     return adata
 
 def normalize_data(adata):
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import analysis_functions as af
-
+import matplotlib.pyplot as plt
+import scanpy as sc
 # Define title
 st.title("Single Cell RNA-Seq Data Analysis")
 
@@ -8,6 +9,7 @@ st.title("Single Cell RNA-Seq Data Analysis")
 st.sidebar.header("Control Panel")
 if st.sidebar.button("Load libraries"):
     af.load_libraries()
+    st.write("scanpy, anndata")
 
 file = st.sidebar.file_uploader("Upload data file")
 if file is not None:
@@ -18,11 +20,19 @@ if st.sidebar.button("Initialize Seurat object"):
     st.write("Seurat object initialized.")
 
 if st.sidebar.button("Perform QC"):
-    adata = af.perform_qc(adata)
+    adata, fig = af.perform_qc(adata, plt, sc)
     st.write("Quality control performed.")
+    st.dataframe(adata.obs)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(fig)
+
+min_genes = st.sidebar.number_input("Minimum genes per cell", min_value=1, max_value=5000, value=200)
+min_cells = st.sidebar.number_input("Minimum cells per gene", min_value=1, max_value=5000, value=3)
+max_genes = st.sidebar.number_input("Maximum genes per cell", min_value=1, max_value=5000, value=2500)
+max_mt = st.sidebar.number_input("Maximum MT percent", min_value=1, max_value=100, value=5)
 
 if st.sidebar.button("Filter data"):
-    adata = af.filter_data(adata)
+    adata = af.filter_data(adata, min_genes, min_cells, max_genes, max_mt)
     st.write("Data filtered.")
 
 if st.sidebar.button("Normalize data"):
@@ -49,5 +59,3 @@ if st.sidebar.button("Cluster data"):
 if st.sidebar.button("Perform non-linear dimensionality reduction"):
     af.perform_non_linear_dimensionality_reduction(adata)
     st.write("Non-linear dimensionality reduction performed.")
-
-# Main panel can be populated by writing to the streamlit display with st.write(), st.dataframe(), etc.
